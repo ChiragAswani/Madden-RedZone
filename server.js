81cd973436f78c2345ccfe13d778365d794a6771
Chirag Aswani <chirag@aswani.net>
2021-06-28 00:11:38 -0400

    immortals

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/client'))
-app.get('/', (req, res) => {return res.sendFile(path.join(__dirname, "../client", "index.html"));});
 
+app.get('/', (req, res) => {return res.sendFile(path.join(__dirname, "../client", "index.html"));});
+app.get("/immortals",function(req, res){
+    res.sendFile(path.join(__dirname + "/client/immortals.html"));
+});
 app.listen(PORT, () => {console.log(`Server listening on port ${PORT}...`);});
