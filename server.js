b263b9ddb30a1ba483fe7576eb5be320802d1eb2
Chirag Aswani <chirag@aswani.net>
2021-07-23 16:35:37 -0400

    ip tracking

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser')
 const express = require('express');
 const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/client'))
 
@@ -10,4 +13,10 @@ app.get('/', (req, res) => {return res.sendFile(path.join(__dirname, "../client"
 app.get("/immortals",function(req, res){
     res.sendFile(path.join(__dirname + "/client/immortals.html"));
 });
+
+app.get("/log-usage",function(req, res){
+    const text = (`Timestamp: ${new Date().getTime()} | IP: ${req.query.ip}`);
+    return res.status(200).send(text);
+});
+
 app.listen(PORT, () => {console.log(`Server listening on port ${PORT}...`);});
